583b43815fc2b4f8c5a7381d734610a66a2e112a
radu122T <radu.cudalb@yahoo.ro>
2022-07-06 20:34:41 +0300

    06.07.2022

--- client/src/screens/Cart.js
+++ client/src/screens/Cart.js
@@ -1,15 +1,26 @@
-import React, { useEffect,useState } from "react"
+import React, { useEffect,useState, useRef } from "react"
 import CartItemForCart from "../components/cartComponents/CartItemForCart"
 import Footer from "./../components/Footer"
 import { connect } from "react-redux"
 import Nav from "../components/Nav"
 import { Link } from "react-router-dom"
+import PaymentForm from "../components/cartComponents/PaymentForm"
+import ShippingForm from "../components/cartComponents/shippingForm"
+
+
 
 const Cart = ({ cart }) => {
     useEffect(()=>{window.scrollTo(0, 0)},[]);
+
+    const [skipCount, setSkipCount] = useState(true);
     const [totalPrice, setTotalPrice] = useState(0)
     const [totalItems, setTotalItems] = useState(0)
     const [isChecked, setIsChecked] = useState(true);
+    const [formData, setFormData] = useState({
+        shipping: { values: null, validated: false },
+        payment: { values: null, validated: false },
+    });
+    const [clicked,setClicked] = useState(false)
 
     useEffect(()=> {
         let items= 0
@@ -19,13 +30,52 @@ const Cart = ({ cart }) => {
             items += item.qty
             price += item.qty * item.price
         })
-
         setTotalPrice(price)
         setTotalItems(items)
     }, [cart,totalPrice, totalItems, setTotalPrice, setTotalItems])
+    
+
     const handleOnChange = () => {
         setIsChecked(!isChecked);
-      }
+    }
+    
+    const shippingref = useRef();
+    const paymentref = useRef();
+    
+    useEffect(() => {
+        if (skipCount) setSkipCount(false);
+        if(!skipCount){
+            if (!formData.shipping.validated) {console.log('nop')}
+            if (formData.shipping.validated && !isChecked) {
+                {if(formData.payment.validated)
+                    {console.log({formData})}
+                else{
+                    console.log('nop2')
+                }
+            }}
+            else if (formData.shipping.validated && isChecked) {
+                console.log({formData})
+            }
+        
+            setClicked(false)
+        }
+       
+    }, [clicked]);
+    
+    async function handleSubmit() {
+        await shippingref.current.Submit();
+        await paymentref.current.Submit();
+        setClicked(true)
+        
+    }
+    
+    function handleChangeshipping(data) {
+        setFormData({ ...formData, shipping: data });
+    }
+    
+    function handleChangepayment(data) {
+        setFormData({ ...formData, payment: data });
+    }
     return (
         
             <div className="cartContainer">
@@ -49,66 +99,25 @@ const Cart = ({ cart }) => {
                                 ))}
                             </div>   
                         </main>
-                        {cart.length>0 && 
-                        <section className="shipping">
-                            <h2>Livrare</h2>
-                            <div className="personalInfo">
-                                <span>email</span>
-                                <input type="text"/>
-                                <span>telefon</span>
-                                <input type="text"/>
-                                <span>nume de familie</span>
-                                <input type="text"/>
-                                <span>prenume</span>
-                                <input type="text"/>
-                                <span>adresa</span>
-                                <input type="text"/>
-                                <div className="onTheSameRow">
-                                    <span className="city">oras</span>
-                                    <input type="text" className="input1"/>
-                                    <span className="state">judet</span>
-                                    <input type="text" className="input2"/>
-                                    <span className="cod">cod postal</span>
-                                    <input type="text" className="input3"/>
-                                </div>
-                        </div>
-                        <h2 className="facturare"><input id="fac" type="checkbox" value="Facturare" checked={isChecked} onChange={handleOnChange}/>Facturare cu aceleasi date </h2>
-                        </section>}
-                        
-                        {cart.length>0 && !isChecked && 
-                        <section className="payment">
-                            <h2>Facturare</h2>
-                            <div className="personalInfo">
-                                <span>nume de familie</span>
-                                <input type="text"/>
-                                <span>prenume</span>
-                                <input type="text"/>
-                                <span>adresa</span>
-                                <input type="text"/>
-                                <div className="onTheSameRow">
-                                    <span className="city">oras</span>
-                                    <input type="text" className="input1"/>
-                                    <span className="state">judet</span>
-                                    <input type="text" className="input2"/>
-                                    <span className="cod">cod postal</span>
-                                    <input type="text" className="input3"/>
-                                </div>
-                            </div>
-                        </section>}
+                    <div className="shippingForms">
+                        {cart.length>0 && <ShippingForm onChange={handleChangeshipping} refId={shippingref} />}
+                        {cart.length>0 && <h2 className="facturare"><input id="fac" type="checkbox" value="Facturare" checked={isChecked} onChange={handleOnChange}/>Facturare cu aceleasi date </h2>}
+                        {cart.length>0 && !isChecked && <PaymentForm onChange={handleChangepayment} refId={paymentref} />}
                     </div>
-                    {cart.length>0 && 
-                    <div className="totalAndButtonCart">  
-                        <div className="totalCart">
-                        <h3>Rezumat comanda</h3>
-                        <ul>{cart.map((x)=>{ return <li key={x.key}>{x.desc} <span>x {x.qty}</span></li>})} </ul>
-                        <span>Pret transport: 20 lei</span>
-                        <span>Pret total: {totalPrice+20} lei</span>
-                    </div>
-                    <div className="paymentButton">
-                        <button className="checkout">Plata cu cardul</button>
-                    </div>
-                    </div>}
                 </div>
+                {cart.length>0 && 
+                <div className="totalAndButtonCart">  
+                    <div className="totalCart">
+                    <h3>Rezumat comanda</h3>
+                    <ul>{cart.map((x)=>{ return <li key={x.key}>{x.desc} <span>x {x.qty}</span></li>})} </ul>
+                    <span>Pret transport: 20 lei</span>
+                    <span>Pret total: {totalPrice+20} lei</span>
+                </div>
+                <div className="paymentButton">
+                    <button onClick={handleSubmit} className="checkout">Plata cu cardul</button>
+                </div>
+                </div>}
+            </div>
             <Footer />
         </div>
     )}
--- server/server.js
+++ server/server.js
@@ -7,8 +7,21 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 app.use(express.static(publicPath2))
+app.use(express.json())
+app.use(express.urlencoded)
+
+
 app.get("*", (req,res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.post('/cart', (req,res) => {
+    const {parce} = req.body
+    console.log(parce)
+    if (!parce) {
+        return res.status(400).send({status:'failed'})
+    }
+    res.status(200).send({status:received})
 })
 
 
